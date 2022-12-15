e0ac981e7c6c3e41868fedc2b6e187c696990a91
paovvr <paolav367@gmail.com>
2022-12-15 05:28:11 -0300

    c

--- blog/estudiantes/urls.py
+++ blog/estudiantes/urls.py
@@ -1,11 +1,10 @@
 
 from django.urls import path
 from estudiantes import views
-#from estudiantes.views import Home
 
 
 urlpatterns =[
     path("",views.index, name="index"),
  ]   
 
-#path("",views.home, name="home"),
+
